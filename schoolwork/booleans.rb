d89d432dea25d9c048bc024de85898cf3a0751f1
admin <admin@admins-MacBook-Pro-3.local>
2015-08-11 14:01:48 +0700

    booleans

---
+++ schoolwork/booleans.rb
@@ -0,0 +1,14 @@
+
+
+
+
+def number_is_big(number)
+	if number > 100
+		puts "number is big"
+	elsif number == 21
+		puts "number is favorite"
+	else 
+		puts "number is not big"
+	end
+end
+number_is_big(100)
