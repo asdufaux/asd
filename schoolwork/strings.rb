b7524e72942b2202a26ff57b1cc9d360ee606d2b
admin <admin@admins-MacBook-Pro-3.local>
2015-08-11 11:11:24 +0700

    makes changes

--- schoolwork/strings.rb
+++ schoolwork/strings.rb
@@ -1,2 +1,16 @@
 words = "i am a string."
 puts words
+
+# plays with strings
+more_words = 'i am another string'
+puts words + more_words
+
+puts 'adds words and more_words'
+most_words = words + more_words
+puts most_words
+
+# these don't work
+# puts 'subtracts words from most_words'
+# puts most_words - words
+puts words.capitalize!
+puts words
