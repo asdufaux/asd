f6488ba8f8668bab12e163f25ab92d172b2359a2
admin <admin@admins-MacBook-Pro-3.local>
2015-08-12 10:10:21 +0700

    ruby hard way

--- homework/learn_ruby_hard_way/exercise006.rb
+++ homework/learn_ruby_hard_way/exercise006.rb
@@ -1,7 +1,7 @@
 x = "There are #{10} types of people."
 binary = "binary"
 do_not = "don't"
-y = "Those who know #{binary} and those who #{do_not}."
+y= "Those who know #{binary} and those who #{do_not}."
 puts x
 puts y
 puts "I said: #{x}."
