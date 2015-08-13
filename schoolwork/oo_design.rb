3a9641f72069a7e1dfa5aa3ef036d3ea6b7f1b97
admin <admin@admins-MacBook-Pro-3.local>
2015-08-13 14:48:13 +0700

    worked on object oriented design

---
+++ schoolwork/oo_design.rb
@@ -0,0 +1,39 @@
+class Car
+	attr_reader :doors, :seats, :butt_warmers
+	def initialize(args)
+		@name = args[:name]
+		@doors = args[:doors]
+		@seats = args[:seats]
+		@wheels = 4
+		@butt_warmers = args[:butt_warmers]
+	end
+	def get_name
+		@name
+	end	
+end
+porsche = Car.new({name: "chickon-mobile", doors: 4, seats: 5, butt_warmers:4})
+# p porsche.doors
+
+honda = Car.new({name: "lemon chickon", doors: 4, seats: 5, butt_warmers: 2})
+
+class Garage
+	def initialize
+		@cars = []
+	end
+
+	
+	def give_car(car)
+		@cars << car
+	end
+	def show_cars
+		@cars.each do |car|
+			p car.get_name
+		end
+	end
+end
+garage = Garage.new
+
+
+garage.give_car(porsche)
+garage.give_car(honda)
+garage.show_cars
