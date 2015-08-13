71c2db76bc573580698f278d3ad1f8608ac42895
descartez <rvanstong@gmail.com>
2015-08-13 11:42:06 -0700

    shows aidan how argv worked (learn ruby hard, exercise013.rb)

--- homework/learn_ruby_hard_way/exercise013.rb
+++ homework/learn_ruby_hard_way/exercise013.rb
@@ -1,6 +1,31 @@
-first, second, third = ARGV
+# first, second, third = ARGV
 
-puts "the script is called: #{$0}"
-puts "your first variable is: #{first}"
-puts "your second variable is: #{second}"
-puts "your third variable is: #{third}"
+# puts "the script is called: #{$0}"
+# puts "your first variable is: #{first}"
+# puts "your second variable is: #{second}"
+# puts "your third variable is: #{third}"
+
+
+# Showing aidan how ARGV works
+whole_argv = ARGV
+
+p whole_argv
+
+command = ARGV[0]
+target = ARGV[1..-1]
+
+def speaker(command, target)
+  p command
+  case command
+  when "yell"
+    puts target.join(" ").upcase
+  when "whisper"
+    puts target.join(" ").downcase
+  when "fasttalk"
+    puts target.join("").downcase
+  else
+    puts "'#{command}' is not a command"
+  end
+end
+
+speaker(command, target)
