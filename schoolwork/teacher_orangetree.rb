3e08f497ff7696ea36da37a45501cec0026a435b
descartez <rvanstong@gmail.com>
2015-08-14 11:01:09 -0700

    adds in methods which determine if the tree is dead

--- schoolwork/teacher_orangetree.rb
+++ schoolwork/teacher_orangetree.rb
@@ -2,6 +2,7 @@ class OrangeTree
   def initialize
     @branches = []
     @height = 0
+    @alive = true
   end
 
   def grow!
@@ -11,10 +12,28 @@ class OrangeTree
     end
   end
 
+  def is_dead?
+    if @height > 120 || @branches.length > 21
+      @alive = false
+    end
+  end
+
+  def is_dead
+      puts "One of your trees has died."
+      check_orange_tree
+  end
+
+  def alive?
+    @alive
+  end
+
   def check_orange_tree
+    puts "."*30
     puts "Orange Tree Health: \n\n"
     puts "Height: #{@height} inches"
-    puts "Number of Oranges: #{@branches.length}"
+    puts "Number of Oranges: #{@branches.length} \n"
+    puts "."*30
+    puts "\n"
   end
 end
 
@@ -27,8 +46,10 @@ end
 
 orange_tree = OrangeTree.new
 
-10.times do
+until orange_tree.alive? == false do
   orange_tree.grow!
+  orange_tree.check_orange_tree
+  orange_tree.is_dead?
 end
 
-orange_tree.check_orange_tree
+orange_tree.is_dead
